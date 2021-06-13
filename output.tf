cbfb3ffead10ab73e0dff952da9159db62c94082
Halil Yavuz <68934275+hyvz-devops@users.noreply.github.com>
2021-06-12 21:00:40 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
+output "private_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
   value       = "aws_instance.example.private_ip"
 }
